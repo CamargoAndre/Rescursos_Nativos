44383be96f05971e80a168f8bc1a1e71e0f26399
Andre de Camargo <camargo_and@hotmail.com>
2020-05-07 16:58:25 -0300

    aula Recursos Nativos 2.5.1

---
+++ telas/ListaDeLugaresTela.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+
+
+const ListaDeLugaresTela = (props) => {
+    return (
+    
+        <View >
+            {ListaDeLugaresTela}
+        </View>
+    
+    );
+}
+
+const estilos = StyleSheet.create({
+
+
+})
+
+export default ListaDeLugaresTela;
---
+++ telas/NovoLugarTela.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+
+
+const NovoLugarTela = (props) => {
+    return (
+    
+        <View >
+            {NovoLugarTela}
+        </View>
+    
+    );
+}
+
+const estilos = StyleSheet.create({
+
+
+})
+
+export default NovoLugarTela;
